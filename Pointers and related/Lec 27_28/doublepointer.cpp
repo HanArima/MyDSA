34857cd13153601d3a15327ad7b820600d35f5b1
HanArima <garhanimasa2003@gmail.com>
2023-08-02 17:23:55 +0530

    Double Pointers

---
+++ Pointers and related/Lec 27_28/doublepointer.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+int update(int **p)
+{
+    // p = p + 1;
+    // kuch change hoga?? - No;
+
+    *p = *p + 1;
+    // kuch change hoga?? -
+
+    //**p = **p + 1;
+    // kuch change hoga?? -
+}
+int main()
+{
+    int a = 5;
+    int *p = &a;
+    int **p2 = &p;
+
+    // cout << "Working Fine" << endl;
+
+    // cout << "Printing a " << a << endl;
+    // cout << "Adress of a " << &a << endl;
+
+    // cout << "Printing p " << p << endl;
+    // cout << "Address of p " << &p << endl;
+
+    // cout << "Printing p2 " << p2 << endl;
+    // cout << "Address of p2 " << &p2 << endl;
+
+    // cout << a << endl;
+    // cout << *p << endl;
+    // cout << **p2 << endl;
+    cout << endl
+         << "Before" << endl;
+    cout << a << endl;
+    cout << p << endl;
+    cout << p2 << endl;
+
+    update(p2);
+
+    cout << endl
+         << "After" << endl;
+    cout << a << endl;
+    cout << p << endl;
+    cout << p2 << endl;
+}
