fbfb4b5d761c194ad12548edf7d9b50de6eac653
HanArima <garhanimasa2003@gmail.com>
2024-02-01 14:15:51 +0530

    Striver Day 01

---
+++ cppbasics.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    //Strings and getline
+
+    /*
+    string s1, s2;
+    cin >> s1 >> s2; 
+    cout << s1 << s2;
+
+    string str;
+    getline(cin, str);
+    cout << "Using the getline function: "<< str;
+    return 0;
+    */    
+
+
+}
